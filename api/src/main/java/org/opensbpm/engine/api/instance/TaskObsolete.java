ae0c53cd1a3e9179908a82cc0989094763aceaf9
Stefan Sedelmaier <stefan@sedelmaier.at>
2022-07-01 14:11:55 +0200

    fix imports

--- api/src/main/java/org/opensbpm/engine/api/instance/TaskObsolete.java
+++ api/src/main/java/org/opensbpm/engine/api/instance/TaskObsolete.java
@@ -10,7 +10,6 @@ import java.util.stream.Collectors;
 import org.opensbpm.engine.api.model.FieldType;
 import static java.lang.String.format;
 import static java.util.Collections.unmodifiableList;
-import static org.opensbpm.engine.utils.StreamUtils.toOne;
 
 @Deprecated
 public class TaskObsolete extends Task {
