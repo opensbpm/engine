5009aef67788095c3b27d0ed764ee9778cd6cc00
Stefan Sedelmaier <stefan@sedelmaier.at>
2022-07-27 18:40:42 +0200

    EngineEventsCollector is a read only collection

--- core/src/test/java/org/opensbpm/engine/core/junit/ServiceITConfig.java
+++ core/src/test/java/org/opensbpm/engine/core/junit/ServiceITConfig.java
@@ -1,22 +1,27 @@
-/*******************************************************************************
+/** *****************************************************************************
  * Copyright (C) 2020 Stefan Sedelmaier
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
- ******************************************************************************/
+ *****************************************************************************
+ */
 package org.opensbpm.engine.core.junit;
 
+import java.io.Serializable;
+import java.util.AbstractCollection;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import org.opensbpm.engine.api.events.EngineEvent;
 import org.opensbpm.engine.core.EngineConfig;
 import org.springframework.context.annotation.Bean;
@@ -36,12 +41,25 @@ public class ServiceITConfig {
         return new EngineEventsCollector();
     }
 
-    public static class EngineEventsCollector extends ArrayList<EngineEvent<?>> {
+    public static class EngineEventsCollector extends AbstractCollection<EngineEvent<? extends Serializable>> {
+
+        private List<EngineEvent<? extends Serializable>> events = new ArrayList<>();
 
         @EventListener
-        public void handleEngineEvent(EngineEvent<?> engineEvent) {
-            add(engineEvent);
+        public void handleEngineEvent(EngineEvent<? extends Serializable> engineEvent) {
+            events.add(engineEvent);
+        }
+
+        @Override
+        public int size() {
+            return events.size();
+        }
+
+        @Override
+        public Iterator<EngineEvent<? extends Serializable>> iterator() {
+            return events.iterator();
         }
 
     }
+
 }
