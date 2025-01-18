58df799862fe7f8148b76db15ba7c1a9f679cdd3
Sedelmaier Stefan <stefan@sedelmaier.at>
2025-01-18 05:50:20 +0100

    Rename stresstest Module to userbot

--- engine/stresstest/src/main/java/org/opensbpm/engine/stresstest/WorkflowOrchestrator.java
+++
@@ -1,8 +0,0 @@
-package org.opensbpm.engine.stresstest;
-
-import org.springframework.context.ConfigurableApplicationContext;
-
-public interface WorkflowOrchestrator {
-     void execute(ConfigurableApplicationContext context) ;
-
-}
